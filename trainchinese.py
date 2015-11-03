0ed60b67d1f17a8675b5c7a4c35545d40988a41b
Alejandro Sánchez <contacto@alsanchez.es>
2015-11-03 22:16:21 +0100

    Actually implement downloading

--- trainchinese.py
+++ trainchinese.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import re
+import sys
 import json
 import html.parser
 import urllib.parse
@@ -26,16 +27,36 @@ def getresults(query):
         match = match.replace("'", "\"")
         match = "[" + match + "]"
         result = json.loads(match)
-        print(result)
         name = result[2]
         name = re.sub(spanRegex, "", name)
         name = html.parser.HTMLParser().unescape(name)
-        audio = "http://www.trainchinese.com/v1/" +result[6][3:]
+        audio = "/v1/" +result[6][3:]
         results.append([name, audio])
 
     return results 
 
-if __name__ == "__main__":
-    for (name, audio) in getresults("六"):
-        print("{}\t{}".format(name, audio))
+def downloadfile(url, path):
+    domain = "www.trainchinese.com"
+    client = http.client.HTTPConnection(domain)
+    client.request("GET", url)
+    response = client.getresponse()
 
+    with open(path, "wb") as outputfile:
+        outputfile.write(response.read())
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print("Usage: {} <word>".format(sys.argv[0]))
+        sys.exit(1)
+
+    word = sys.argv[1]
+
+    for (name, audio) in getresults(word):
+        if name == word:
+            downloadfile(audio, name + ".mp3")
+            print("Done")
+            sys.exit(0)
+
+    print("Not found")
+    sys.exit(1)
